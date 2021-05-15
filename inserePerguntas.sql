159e80d48e5eeed560c337d06576f0a42892538c
Douglas Giacomelli <douglasgia@hotmail.com>
2021-05-15 18:25:45 -0300

    atualização

--- inserePerguntas.sql
+++ inserePerguntas.sql
@@ -1,26 +1,31 @@
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu estou satisfeito de viver em Caxias do Sul.');
+VALUES (NULL, '1', '1', '1- Eu estou satisfeito de viver em Caxias do Sul.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu estou satisfeito com a vida que levo');
+VALUES (NULL, '1', '1', '2- Eu estou satisfeito com a vida que levo');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', ' Eu estou satisfeito com o lugar em que moro.
+VALUES (NULL, '1', '1', '3- Eu estou satisfeito com o lugar em que moro.
 ');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu estou satisfeito com o sistema de Saúde de Caxias do Sul  
-(Infraestrutura/Atendimento).
-');
+VALUES (NULL, '1', '1', ' 4- Eu estou satisfeito com o sistema de Saúde de Caxias do Sul  
+(Infraestrutura/Atendimento).');
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu pratico alguma atividade física com frequência.');
+VALUES (NULL, '1', '1', '5- Eu pratico alguma atividade física com frequência.');
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu caminho pela cidade com frequência.');
+VALUES (NULL, '1', '1', '6- Eu caminho pela cidade com frequência.');
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu me sinto integrado à cidade de Caxias do Sul.');
+VALUES (NULL, '1', '1', '7- Eu me sinto integrado à cidade de Caxias do Sul.');
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu estou satisfeito com a minha atual situação de trabalho.');
+VALUES (NULL, '1', '1', '8- Eu estou satisfeito com a minha atual situação de trabalho.');
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', 'Eu estou satisfeito com a situação financeira das pessoas que vivem comigo.');
+VALUES (NULL, '1', '1', '9- Eu estou satisfeito com a situação financeira das pessoas que vivem comigo.');
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', ' Eu estou satisfeito com o comércio existente nas proximidades de onde moro.');
+VALUES (NULL, '1', '1', '10- Eu estou satisfeito com o comércio existente nas proximidades de onde moro.');
