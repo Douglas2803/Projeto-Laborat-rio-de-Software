e7f0f3c989aa26ca36ed7f6cfb264f99abe19be3
aprezzi <aprezzi@ucs.br>
2021-05-15 17:44:31 -0300

    index

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     utilizados para pesquisas por terceiros de forma anônima.
     </p>
     <hr class="my-4">
-    <p>Continuação </p>
+
     <p class="lead">
         <a class="btn btn-success btn-lg" href="questionarios.php" role="button">Aceito</a>
         <a class="btn btn-danger btn-lg" href="saida.php" role="button">Não aceito</a>
