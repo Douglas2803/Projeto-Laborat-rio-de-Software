08e1f4685f3503d7c68d5ea50cf3f900cfe0f4d2
aprezzi <aprezzi@ucs.br>
2021-05-15 18:04:41 -0300

    insere perguntas

---
+++ inserePerguntas.sql
@@ -0,0 +1,8 @@
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '1', '1', 'Eu estou satisfeito de viver em Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '1', '1', 'Eu estou satisfeito com a vida que levo');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '1', '1', 'Eu estou satisfeito com o lugar em que moro.');
