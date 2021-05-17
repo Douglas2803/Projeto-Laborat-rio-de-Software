b14e613a358ff7b37182cb60a277b88a25c86d41
Douglas Giacomelli <douglasgia@hotmail.com>
2021-05-17 18:12:04 -0300

    script sql formulário 2

--- inserePerguntas.sql
+++ inserePerguntas.sql
@@ -1,3 +1,5 @@
+--Questionário 1
+
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '1', '1', '1- Eu estou satisfeito de viver em Caxias do Sul.');
 
@@ -5,8 +7,7 @@ INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`
 VALUES (NULL, '1', '1', '2- Eu estou satisfeito com a vida que levo');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '1', '1', '3- Eu estou satisfeito com o lugar em que moro.
-');
+VALUES (NULL, '1', '1', '3- Eu estou satisfeito com o lugar em que moro.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '1', '1', ' 4- Eu estou satisfeito com o sistema de Saúde de Caxias do Sul  
@@ -29,3 +30,41 @@ VALUES (NULL, '1', '1', '9- Eu estou satisfeito com a situação financeira das 
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '1', '1', '10- Eu estou satisfeito com o comércio existente nas proximidades de onde moro.');
+
+--Questionário 2
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '1- No geral, os serviços de ônibus/lotação da Cidade de Caxias do Sul são satisfatórios.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '2- A frequência dos ônibus/lotações de Caxias do Sul é adequada às minhas necessidades.
+');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '3- Os ônibus/lotações são pontuais.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '4- É possível realizar mais de uma viagem pagando apenas uma tarifa por meio da passagem integrada.
+');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '5- O valor das tarifas de ônibus/lotação é acessível.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '6- Eu me sinto seguro nos terminais e nas paradas de ônibus/lotação de Caxias do Sul.
+');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '7- Eu me sinto seguro dentro dos ônibus/lotações de Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '8- Os ônibus/lotações de Caxias do Sul são confortáveis.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '9- Os terminais e paradas de ônibus/lotações de Caxias do Sul são confortáveis.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '10- É fácil obter informações a respeito dos serviços de ônibus em Caxias do Sul.');
+
+
+
