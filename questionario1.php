8ac4a17f10a31a3c649b3eabee5dfda06bb05a93
aprezzi <aprezzi@ucs.br>
2021-05-23 16:18:56 -0300

    Add files via upload

--- questionario1.php
+++ questionario1.php
@@ -36,31 +36,59 @@ $rotinas = new rotinas();
     <div class="row">
         <div class="container">
         <?php 
-        $perguntas=$rotinas->selecionar_where("perguntas","pergunta","id_questionario",1);
+        $perguntas=$rotinas->selecionar_where("perguntas","*","id_questionario",1);
        
         ?>
             <form method="POST" action="gravar_questionario.php">
             <div class="form-group">
                 <?php
                 while($linha = mysql_fetch_assoc($perguntas)){
+                    $id_pergunta = $linha['id_pergunta'];
                 ?>
                 <p><?php echo utf8_encode($linha['pergunta']); ?> </p>
                 <div class="row">
                     <?php
                         $alternativas=$rotinas->selecionar("alternativas","*","","");
-                        $i=0;
-                        while($linha = mysql_fetch_assoc($alternativas)){   
-                            $i++;      
+                        
+                        while($linha1 = mysql_fetch_assoc($alternativas)){   
+                         $id_alternativa = $linha1['id_alternativas'] ; 
+                         
                     ?>
                         <div class="form-check-inline ml-3">
                             <p class="form-check-label">
-                                <input type="radio" class="form-check-input" name="pergunta<?{$i}?>" id="pergunta<?{$i}?>"  /> 
-                                <?php echo $linha['descricao']; ?>
+                                <input type="radio" class="form-check-input" name="alternativa-<?php echo $id_pergunta; ?>" 
+                                id="alternativa-<?php echo $id_pergunta; ?>-<?php echo $id_alternativa?>"  /> 
+                                <?php echo $linha1['descricao']; ?>
                             </p>
                         </div>
+                        
+                        <script>
+                            $(document).ready(function () {
+                                var perguntas = [];
+                                // var alternativas = [];
+                                $('#alternativa-<?php echo $id_pergunta; ?>-<?php echo $id_alternativa?>').change(function() {
+                                    var currentId = $(this).attr('id');
+                                    var result = currentId.split('-');
+                                    var pergunta = result[1];
+                                    var alternativa = result[2];
+                                    
+                                    perguntas.push({ Pergunta: pergunta });
+                                    
+                                     // alert(perguntas[0]);
+                                    // for(let i = 0; i < perguntas.length; i++){
+                                    //     console.log(perguntas[i]);
+                                    // }
+                                    // alternativas.push({ Alternativa: alternativa });
+                                    // alternativas.forEach(item => alert(item))
+                                });
+                               
+                             });
+                        </script>
                     <?php
                     } // fecha alternativas
+                    
                     ?>
+                    
                 </div>
                 <?php
                 } //fecha perguntas
@@ -70,14 +98,14 @@ $rotinas = new rotinas();
             <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
             </div> 
             <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>  
+                <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>  
             <button type="submit" class="btn btn-primary">Responder</button>  
            </form>
         </div>
