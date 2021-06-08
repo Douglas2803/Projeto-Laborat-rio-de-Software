c3baa8f699626771dad649b980ff7a14c2751c02
aprezzi <aprezzi@ucs.br>
2021-06-07 21:07:28 -0300

    Add files via upload

--- gravar_questionario.php
+++ gravar_questionario.php
@@ -1,3 +1,33 @@
 <?php
-echo 'gravar questionario';
+
+
+include("banco/rotinas.php"); 
+$rotinas = new rotinas();
+
+$perguntas = $_POST['perguntas_id'];
+
+$auxPerguntas = explode(',', $perguntas);
+$ip_ =  $_SERVER["REMOTE_ADDR"];
+
+foreach($auxPerguntas as $aux){
+    $tipos=$rotinas->selecionar_where("perguntas","*","id_pergunta",$aux);
+    $linha = mysql_fetch_assoc($tipos);
+    $tipo = $linha['id_tipo'];
+
+   $pergunta  = $_POST['pergunta_selecionada_'.$aux];
+   $alternativa  = $_POST['alternativa_selecionada_'.$aux];
+   if($tipo == 1){
+    $resultado=$rotinas->inserir("respostas ","","NULL,'$pergunta', '$alternativa','','$ip_'");
+   }else{
+    echo 'outro insert';   
+   }
+   
+} 
+  
+
+header("location:index.php");
+
+
+// var_dump($_POST);
+// die;
 ?>
