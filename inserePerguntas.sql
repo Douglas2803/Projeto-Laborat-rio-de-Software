67e785f87cd479e974d72ada0cbb281fb9d8fdd6
Douglas Giacomelli <douglasgia@hotmail.com>
2021-06-12 11:15:15 -0300

    perguntas 1

--- inserePerguntas.sql
+++ inserePerguntas.sql
@@ -31,28 +31,200 @@ VALUES (NULL, '1', '1', '9- Eu estou satisfeito com a situação financeira das 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '1', '1', '10- Eu estou satisfeito com o comércio existente nas proximidades de onde moro.');
 
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '11- É fácil encontrar trabalho em Caxias do Sul');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '12- Eu estou satisfeito com o sistema educacional de Caxias do Sul 
+(Ensino/Infraestrutura).');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '13- Eu estou satisfeito com a qualidade do ar de Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '14- Eu estou satisfeito com o nível de barulho em Caxias do Sul');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '15 - 
+Eu estou empenhado em lutar contra as mudanças climáticas com práticas 
+sustentáveis (separar o lixo, poupar água, dar preferência para produtos 
+recicláveis).
+');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '16 - Eu estou satisfeito com as opções culturais oferecidas na cidade de Caxias 
+do Sul (teatro, música, arte, dança, feiras livres e cinema)');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '17 -
+Eu estou satisfeito com os espaços abertos de lazer oferecidas na cidade de 
+Caxias do Sul (praças e parques).');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '18 - Eu estou satisfeito com os espaços para a prática de esportes em Caxias do 
+Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '19 - Eu estou satisfeito com os espaços abertos de lazer oferecidos no seu Bairro.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '20 - Eu frequento as áreas abertas na minha cidade (praças e parques).');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '21 - Eu frequento atividades culturais na minha cidade');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '22 - As feiras ao ar livre atendem as minhas necessidade');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '23 - Eu estou satisfeito com a conservação dos edifícios em Caxias do Sul.
+');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '24 - É fácil encontrar uma boa habitação por um preço razoável em Caxias do
+Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '25 - Eu estou satisfeito com a limpeza de Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '26 - Os serviços de coleta e tratamento de lixo atendem seu bairro de forma 
+eficiente.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '27 - Eu estou satisfeito com as ruas em Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '28 - Eu estou satisfeito com as calçadas em Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '29 - Eu estou satisfeito com o transporte público em Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '30 - Eu estou satisfeito com o transporte para outras cidades partindo de Caxias 
+do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '31 - Eu estou satisfeito com a distância entre as paradas de transporte público.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '32 - Eu estou satisfeito com a qualidade das paradas do transporte público.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '33 - Eu estou satisfeito com a facilidade de acesso do meu bairro aos pontos 
+importantes de Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '34 - Eu estou satisfeito com meus percursos a pé na cidade.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '35 - Eu estou satisfeito com a sinalização para pedestres na cidade.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '36 - Os serviços de energia elétrica da prefeitura de Caxias do Sul ajudam as 
+pessoas de forma eficiente');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '37 - Os serviços dos Bombeiros de Caxias do Sul ajudam as pessoas de forma 
+eficiente.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '38 -
+De maneira geral, os serviços de segurança (polícia militar, civil e trânsito) da 
+prefeitura de Caxias do Sul ajudam as pessoas de forma eficiente/adequada. ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '39 - Eu me considero participativo nas decisões públicas relacionadas à cidade 
+(cobrando dos gestores, participando de audiências públicas).');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '40 - De modo geral, a administração pública de Caxias do Sul é confiável.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '41 - Eu me sinto seguro em Caxias do Sul.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '42 - Eu me sinto seguro no meu bairro. ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '43 - De modo geral, a maioria das pessoas em Caxias do Sul é confiável.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '44 - De modo geral, a maioria das pessoas no meu bairro é confiável.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '45 - O serviço de saneamento básico (tratamento de esgoto) atende meu bairro 
+de forma eficiente. ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '46 - O abastecimento de água atende Caxias do Sul de forma eficiente.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '47 - A presença de imigrantes (país, cidade) é bom para Caxias do Sul. ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '48 - Os programas sociais de Caxias do Sul atuam de forma eficiente.');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '49 - Os imigrantes influenciam de forma positiva a economia da nossa cidade. ');
+
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '50 - Gênero (sexo): ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '2', '51 - Idade: ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '52 - Qual seu grau de instrução?');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '53 - Qual seu status de emprego/remuneração?');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '2', '54 - Com quantas pessoas você mora?');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '55 - Quantas pessoas contribuem para a sua renda familiar?');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '56 - Qual a renda total familiar (considerando as pessoas que moram com você)(Valor base ano 2018,Nacional)');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '2', '57 - Onde você mora? (bairro ou ponto de referência)');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '58 - Qual seu tempo de residência no local informado? ');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '2', '59 - Quantos veículos possuem na sua residência?');
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '60) Qual o meio de transporte que você mais utiliza?');
+
+
+
+
+
 --Questionário 2
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '2', '1', '1- No geral, os serviços de ônibus/lotação da Cidade de Caxias do Sul são satisfatórios.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '2', '1', '2- A frequência dos ônibus/lotações de Caxias do Sul é adequada às minhas necessidades.
-');
+VALUES (NULL, '2', '1', '2- A frequência dos ônibus/lotações de Caxias do Sul é adequada às minhas necessidades.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '2', '1', '3- Os ônibus/lotações são pontuais.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '2', '1', '4- É possível realizar mais de uma viagem pagando apenas uma tarifa por meio da passagem integrada.
-');
+VALUES (NULL, '2', '1', '4- É possível realizar mais de uma viagem pagando apenas uma tarifa por meio da passagem integrada.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '2', '1', '5- O valor das tarifas de ônibus/lotação é acessível.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
-VALUES (NULL, '2', '1', '6- Eu me sinto seguro nos terminais e nas paradas de ônibus/lotação de Caxias do Sul.
-');
+VALUES (NULL, '2', '1', '6- Eu me sinto seguro nos terminais e nas paradas de ônibus/lotação de Caxias do Sul.');
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '2', '1', '7- Eu me sinto seguro dentro dos ônibus/lotações de Caxias do Sul.');
@@ -65,6 +237,26 @@ VALUES (NULL, '2', '1', '9- Os terminais e paradas de ônibus/lotações de Caxi
 
 INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
 VALUES (NULL, '2', '1', '10- É fácil obter informações a respeito dos serviços de ônibus em Caxias do Sul.');
+
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '');
+
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '');
+
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '');
+
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '');
+
+
+INSERT INTO `perguntas` (`id_pergunta`, `id_questionario`, `id_tipo`, `pergunta`) 
+VALUES (NULL, '2', '1', '');
 
 
 
