a354be5e7dee2acd870880c8272ede0198aaea93
Douglas Giacomelli <douglasgia@hotmail.com>
2021-06-13 17:07:08 -0300

    alternativas

---
+++ alternativas_61_a_111.sql
@@ -0,0 +1,203 @@
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alternativa`)
+VALUES (NULL, '61', '1'), (NULL, '61', '2'), (NULL, '61', '3'), (NULL, '61', '4'), (NULL, '61', '5'), 
+(NULL, '61', '6'), (NULL, '61', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '62', '1'), (NULL, '62', '2'), (NULL, '62', '3'), (NULL, '62', '4'), (NULL, '62', '5'), 
+(NULL, '62', '6'), (NULL, '62', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '63', '1'), (NULL, '63', '2'), (NULL, '63', '3'), (NULL, '63', '4'), (NULL, '63', '5'), 
+(NULL, '63', '6'), (NULL, '63', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '64', '1'), (NULL, '64', '2'), (NULL, '64', '3'), (NULL, '64', '4'), (NULL, '64', '5'), 
+(NULL, '64', '6'), (NULL, '64', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '65', '1'), (NULL, '65', '2'), (NULL, '65', '3'), (NULL, '65', '4'), (NULL, '65', '5'), 
+(NULL, '65', '6'), (NULL, '65', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '66', '1'), (NULL, '66', '2'), (NULL, '66', '3'), (NULL, '66', '4'), (NULL, '66', '5'), 
+(NULL, '66', '6'), (NULL, '66', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '67', '1'), (NULL, '67', '2'), (NULL, '67', '3'), (NULL, '67', '4'), (NULL, '67', '5'), 
+(NULL, '67', '6'), (NULL, '67', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '68', '1'), (NULL, '68', '2'), (NULL, '68', '3'), (NULL, '68', '4'), (NULL, '68', '5'), 
+(NULL, '68', '6'), (NULL, '68', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '69', '1'), (NULL, '69', '2'), (NULL, '69', '3'), (NULL, '69', '4'), (NULL, '69', '5'), 
+(NULL, '69', '6'), (NULL, '69', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '70', '1'), (NULL, '70', '2'), (NULL, '70', '3'), (NULL, '70', '4'), (NULL, '70', '5'), 
+(NULL, '70', '6'), (NULL, '70', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '71', '1'), (NULL, '71', '2'), (NULL, '71', '3'), (NULL, '71', '4'), (NULL, '71', '5'), 
+(NULL, '71', '6'), (NULL, '71', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '72', '1'), (NULL, '72', '2'), (NULL, '72', '3'), (NULL, '72', '4'), (NULL, '72', '5'), 
+(NULL, '72', '6'), (NULL, '72', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '73', '1'), (NULL, '73', '2'), (NULL, '73', '3'), (NULL, '73', '4'), (NULL, '73', '5'), 
+(NULL, '73', '6'), (NULL, '73', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '74', '1'), (NULL, '74', '2'), (NULL, '74', '3'), (NULL, '74', '4'), (NULL, '74', '5'), 
+(NULL, '74', '6'), (NULL, '74', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '75', '1'), (NULL, '75', '2'), (NULL, '75', '3'), (NULL, '75', '4'), (NULL, '75', '5'), 
+(NULL, '75', '6'), (NULL, '75', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '76', '1'), (NULL, '76', '2'), (NULL, '76', '3'), (NULL, '76', '4'), (NULL, '76', '5'), 
+(NULL, '76', '6'), (NULL, '76', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '77', '1'), (NULL, '77', '2'), (NULL, '77', '3'), (NULL, '77', '4'), (NULL, '77', '5'), 
+(NULL, '77', '6'), (NULL, '77', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '78', '1'), (NULL, '78', '2'), (NULL, '78', '3'), (NULL, '78', '4'), (NULL, '78', '5'), 
+(NULL, '78', '6'), (NULL, '78', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '79', '1'), (NULL, '79', '2'), (NULL, '79', '3'), (NULL, '79', '4'), (NULL, '79', '5'), 
+(NULL, '79', '6'), (NULL, '79', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '80', '1'), (NULL, '80', '2'), (NULL, '80', '3'), (NULL, '80', '4'), (NULL, '80', '5'), 
+(NULL, '80', '6'), (NULL, '80', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '81', '1'), (NULL, '81', '2'), (NULL, '81', '3'), (NULL, '81', '4'), (NULL, '81', '5'), 
+(NULL, '81', '6'), (NULL, '81', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '82', '1'), (NULL, '82', '2'), (NULL, '82', '3'), (NULL, '82', '4'), (NULL, '82', '5'), 
+(NULL, '82', '6'), (NULL, '82', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '83', '1'), (NULL, '83', '2'), (NULL, '83', '3'), (NULL, '83', '4'), (NULL, '83', '5'), 
+(NULL, '83', '6'), (NULL, '83', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '84', '1'), (NULL, '84', '2'), (NULL, '84', '3'), (NULL, '84', '4'), (NULL, '84', '5'), 
+(NULL, '84', '6'), (NULL, '84', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '85', '1'), (NULL, '85', '2'), (NULL, '85', '3'), (NULL, '85', '4'), (NULL, '85', '5'), 
+(NULL, '85', '6'), (NULL, '85', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '86', '1'), (NULL, '86', '2'), (NULL, '86', '3'), (NULL, '86', '4'), (NULL, '86', '5'), 
+(NULL, '86', '6'), (NULL, '86', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '87', '1'), (NULL, '87', '2'), (NULL, '87', '3'), (NULL, '87', '4'), (NULL, '87', '5'), 
+(NULL, '87', '6'), (NULL, '87', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '88', '1'), (NULL, '88', '2'), (NULL, '88', '3'), (NULL, '88', '4'), (NULL, '88', '5'), 
+(NULL, '88', '6'), (NULL, '88', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '89', '1'), (NULL, '89', '2'), (NULL, '89', '3'), (NULL, '89', '4'), (NULL, '89', '5'), 
+(NULL, '89', '6'), (NULL, '89', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '90', '1'), (NULL, '90', '2'), (NULL, '90', '3'), (NULL, '90', '4'), (NULL, '90', '5'), 
+(NULL, '90', '6'), (NULL, '90', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '91', '1'), (NULL, '91', '2'), (NULL, '91', '3'), (NULL, '91', '4'), (NULL, '91', '5'), 
+(NULL, '91', '6'), (NULL, '91', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '92', '1'), (NULL, '92', '2'), (NULL, '92', '3'), (NULL, '92', '4'), (NULL, '92', '5'), 
+(NULL, '92', '6'), (NULL, '92', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '93', '1'), (NULL, '93', '2'), (NULL, '93', '3'), (NULL, '93', '4'), (NULL, '93', '5'), 
+(NULL, '93', '6'), (NULL, '93', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '94', '1'), (NULL, '94', '2'), (NULL, '94', '3'), (NULL, '94', '4'), (NULL, '94', '5'), 
+(NULL, '94', '6'), (NULL, '94', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '95', '1'), (NULL, '95', '2'), (NULL, '95', '3'), (NULL, '95', '4'), (NULL, '95', '5'), 
+(NULL, '95', '6'), (NULL, '95', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '96', '1'), (NULL, '96', '2'), (NULL, '96', '3'), (NULL, '96', '4'), (NULL, '96', '5'), 
+(NULL, '96', '6'), (NULL, '96', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '97', '1'), (NULL, '97', '2'), (NULL, '97', '3'), (NULL, '97', '4'), (NULL, '97', '5'), 
+(NULL, '97', '6'), (NULL, '97', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '98', '1'), (NULL, '98', '2'), (NULL, '98', '3'), (NULL, '98', '4'), (NULL, '98', '5'), 
+(NULL, '98', '6'), (NULL, '98', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '99', '1'), (NULL, '99', '2'), (NULL, '99', '3'), (NULL, '99', '4'), (NULL, '99', '5'), 
+(NULL, '99', '6'), (NULL, '99', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '100', '1'), (NULL, '100', '2'), (NULL, '100', '3'), (NULL, '100', '4'), (NULL, '100', '5'), 
+(NULL, '100', '6'), (NULL, '100', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '101', '1'), (NULL, '101', '2'), (NULL, '101', '3'), (NULL, '101', '4'), (NULL, '101', '5'), 
+(NULL, '101', '6'), (NULL, '101', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '102', '1'), (NULL, '102', '2'), (NULL, '102', '3'), (NULL, '102', '4'), (NULL, '102', '5'), 
+(NULL, '102', '6'), (NULL, '102', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '103', '1'), (NULL, '103', '2'), (NULL, '103', '3'), (NULL, '103', '4'), (NULL, '103', '5'), 
+(NULL, '103', '6'), (NULL, '103', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '104', '1'), (NULL, '104', '2'), (NULL, '104', '3'), (NULL, '104', '4'), (NULL, '104', '5'), 
+(NULL, '104', '6'), (NULL, '104', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '105', '1'), (NULL, '105', '2'), (NULL, '105', '3'), (NULL, '105', '4'), (NULL, '105', '5'), 
+(NULL, '105', '6'), (NULL, '105', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '106', '1'), (NULL, '106', '2'), (NULL, '106', '3'), (NULL, '106', '4'), (NULL, '106', '5'), 
+(NULL, '106', '6'), (NULL, '106', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '107', '1'), (NULL, '107', '2'), (NULL, '107', '3'), (NULL, '107', '4'), (NULL, '107', '5'), 
+(NULL, '107', '6'), (NULL, '107', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '108', '1'), (NULL, '108', '2'), (NULL, '108', '3'), (NULL, '108', '4'), (NULL, '108', '5'), 
+(NULL, '108', '6'), (NULL, '108', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '109', '1'), (NULL, '109', '2'), (NULL, '109', '3'), (NULL, '109', '4'), (NULL, '109', '5'), 
+(NULL, '109', '6'), (NULL, '109', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '110', '1'), (NULL, '110', '2'), (NULL, '110', '3'), (NULL, '110', '4'), (NULL, '110', '5'), 
+(NULL, '110', '6'), (NULL, '110', '7');
+
+INSERT INTO `perguntas_alternativas` (`id_alt_resp`, `id_pergunta`, `id_alterna tiva`)
+VALUES (NULL, '111', '1'), (NULL, '111', '2'), (NULL, '111', '3'), (NULL, '111', '4'), (NULL, '111', '5'), 
+(NULL, '111', '6'), (NULL, '111', '7');
